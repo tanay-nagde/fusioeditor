bfffcf8f483711e3f4359fad91ab8356a6c0dd90
tanay nagde <tanaynagde@gmail.com>
2025-06-01 15:21:17 +0530

    Update firebase.js

--- fusio-drawingtool/firebase.js
+++ fusio-drawingtool/firebase.js
@@ -7,16 +7,7 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB28b_R9ZtVEjfVocPdoPnUq3ZdYeBk7YM",
-  authDomain: "fusio-realtimecanvas.firebaseapp.com",
-  databaseURL: "https://fusio-realtimecanvas-default-rtdb.firebaseio.com",
-  projectId: "fusio-realtimecanvas",
-  storageBucket: "fusio-realtimecanvas.firebasestorage.app",
-  messagingSenderId: "210348306091",
-  appId: "1:210348306091:web:a85a2bf2f82e0c1ea891ee",
-  measurementId: "G-6FLNZ6HEQG"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
